e5a3b45b237dbb42e3303e6b343bb981a8a7a42c
Eike Stremlau <eike.stremlau@gmail.com>
2024-03-17 12:08:09 +0100

    feat: platinum currency

--- src/app/shared/pipes/currency.pipe.ts
+++ src/app/shared/pipes/currency.pipe.ts
@@ -8,6 +8,7 @@ export class CurrencyPipe implements PipeTransform {
 
     transform(value: number, factor: number): ICurrency {
         const currency: ICurrency = {
+            platinum: 0,
             gold: 0,
             silver: 0,
             copper: 0
@@ -16,7 +17,8 @@ export class CurrencyPipe implements PipeTransform {
         /* eslint-disable @typescript-eslint/typedef */
         ({ currency: currency.copper, remainder: value } = this.calc(value, factor));
         ({ currency: currency.silver, remainder: value } = this.calc(value, factor));
-        currency.gold = value;
+        ({ currency: currency.gold, remainder: value } = this.calc(value, factor));
+        currency.platinum = value;
         /* eslint-enable @typescript-eslint/typedef */
 
         return currency;
